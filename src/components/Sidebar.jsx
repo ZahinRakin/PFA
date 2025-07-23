2f262d3e77d984f9876f8b51ec51d4823fdb350a
ZahinRakin <zahinabdullahrakin@gmail.com>
2025-07-23 15:11:45 +0600

    sidebar complete. - svgr - fill = "currentColor" - padding overflowed then the blue indicator got overshadowed. cause: p-12 in the ul element.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,44 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
-import {ReactComponent as iconOverview} from "@/assets/images/icon-nav-overview.svg" //vs code is marking these codes as unused in the code
-import {ReactComponent as iconTransactions} from "@/assets/images/icon-nav-transactions.svg"
-import {ReactComponent as iconBudgets} from "@/assets/images/icon-nav-budgets.svg"
-import {ReactComponent as iconPots} from "@/assets/images/icon-nav-pots.svg"
-import {ReactComponent as iconRecurring} from "@/assets/images/icon-nav-recurring-bills.svg"
+import Button from '@components/Button.jsx'
+
+import IconOverview from '@/assets/images/icon-nav-overview.svg?react'
+import IconTransactions from '@/assets/images/icon-nav-transactions.svg?react'
+import IconBudgets from '@/assets/images/icon-nav-budgets.svg?react'
+import IconPots from '@/assets/images/icon-nav-pots.svg?react'
+import IconRecurring from '@/assets/images/icon-nav-recurring-bills.svg?react'
+import LargeLogo from '@/assets/images/logo-large.svg?react'
+import SmallLogo from '@/assets/images/logo-small.svg?react'
+import IconMinimize from '@/assets/images/icon-minimize-menu.svg?react'
 
 
 const Sidebar = ()=>{
+  const [miniSidebar, setMiniSidebar] = useState(false)
   const [activeTab, setActiveTab] = useState("overview")
   const contents = [
-    { placeholder: "Overview", icon: (isActive)=><iconOverview className={`h-7 w-7 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" }, 
-    { placeholder: "Transactions", icon: (isActive)=><iconTransactions className={`h-7 w-7 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
-    { placeholder: "Budgets", icon: (isActive)=><iconBudgets className={`h-7 w-7 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
-    { placeholder: "Pots", icon: (isActive)=><iconPots className={`h-7 w-7 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
-    { placeholder: "Recurring Bills", icon: (isActive)=><iconRecurring className={`h-7 w-7 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
+    { placeholder: "Overview", icon: (isActive)=><IconOverview className={`h-5 w-5 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" }, 
+    { placeholder: "Transactions", icon: (isActive)=><IconTransactions className={`h-5 w-5 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
+    { placeholder: "Budgets", icon: (isActive)=><IconBudgets className={`h-5 w-5 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
+    { placeholder: "Pots", icon: (isActive)=><IconPots className={`h-5 w-5 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
+    { placeholder: "Recurring Bills", icon: (isActive)=><IconRecurring className={`h-5 w-5 ${isActive? "text-blue-500":"text-white"}`}/>, className: "", link: "#" },
   ]
 
   const renderElem = (content) => {
     const tab = content.placeholder.toLowerCase()
     const isActive = tab === activeTab
-    const design_for_activetab = "bg-white text-gray-900 rounded-r-lg"
+    const design_for_activetab = !miniSidebar ? "bg-white text-gray-900 rounded-r-lg w-full" : ""
     const general_tab_design = `text-white ${content.className}`
     return(
-      <li className="pr-8">
+      <li className={`flex items-center h-12 `}> {/**${!miniSidebar ? "" : ""} */}
+        <div className={`h-full w-1.5 ${isActive ? "bg-blue-700" : "bg-transparent"}`}></div> {/**this div isn't showing after i minimize */}
         <Link 
           to={content.link} 
           onClick={()=>handleActiveTab(tab)} 
-          className={`flex justify-start items-center gap-4 px-8 h-12 ${activeTab!==tab?general_tab_design: design_for_activetab}`}
+          className={`flex justify-start items-center px-6 gap-4 h-full ${activeTab!==tab?general_tab_design: design_for_activetab}`}
         >
           {content.icon(isActive)} 
-          {content.placeholder}
+          {!miniSidebar ? content.placeholder : ""}
         </Link>
       </li>
     )
@@ -41,12 +48,38 @@ const Sidebar = ()=>{
     setActiveTab(activeTabName)
   }
 
+  const handleMinimize = () => {
+    setMiniSidebar((p) => !p)
+  }
+
+  const buttonDesign = "px-7 border-0 hover:bg-transparent bg-amber-500 text-white w-full h-12"
+  const logoDesign = "px-7 pt-10 pb-15 flex items-center justify-start"
+
   return (
-    <aside>
-      
-      <ul className="flex-col bg-gray-900 text-white h-screen w-70">
+    <aside className={`overflow-hidden h-screen bg-gray-900 text-white rounded-r-2xl flex flex-col justify-start ${!miniSidebar ? "w-70":"w-20"} `}>
+      {!miniSidebar ? (
+        <section className={logoDesign}>
+          <LargeLogo className="hover:text-blue-500"/>
+        </section>):(
+        <section className={logoDesign}>
+          <SmallLogo className="hober:text-blue-500"/>
+        </section>
+      )}
+      <ul className={`flex flex-col w-full flex-grow ${!miniSidebar ? "pr-12 " : "pr-2"}`}>
         {contents.map(content => renderElem(content))}
       </ul>
+      <Button 
+        children={
+          <div className="flex justify-start gap-4 w-full h-5 items-center">
+            <IconMinimize className={`h-full  ${miniSidebar?"rotate-180":""}`}/>
+            {!miniSidebar?(<span className="flex h-full w-full items-center">
+              Minimize Menu
+            </span>):""}
+          </div>
+        }
+        onClick={handleMinimize}
+        className={buttonDesign}
+      />
     </aside>
   )
 }
